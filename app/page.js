3d8c62e3822b7b63e40ab40dd47a90854c142f8d
Hebert <hebertdev82@gmail.com>
2025-05-03 17:46:34 -0300

    feat: initial page

---
+++ app/page.js
@@ -0,0 +1,11 @@
+export default function Home() {
+  return (
+    <>
+      <p className="text-3xl text-center font-bold justify-center mt-31">
+        Bolão de Dezenas
+      </p>
+
+      <p className="text-center">Em construção!</p>
+    </>
+  );
+}
